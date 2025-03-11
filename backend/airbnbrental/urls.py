fbba119dacfc6d5eadc596b15c79afc5b9bc4c57
Charles Walton <pythontutorials36@gmail.com>
2025-03-11 07:47:51 +0100

    Refactor code in the url that caused main url from working with api, due to name collission

--- backend/airbnbrental/urls.py
+++ backend/airbnbrental/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', include('userauths.urls')),
     #path("useradmin/", include("useradmin.urls")),
 
-    # path("ckeditor/", include("ckeditor_uploader.urls")),
+    path("ckeditor/", include("ckeditor_uploader.urls")),
     #path("ckeditor5/", include('django_ckeditor_5.urls')),
 ]
 
