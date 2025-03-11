9a46134c7c0b16945fba294cac0eef006c248420
Charles Walton <pythontutorials36@gmail.com>
2025-03-11 17:08:26 +0100

    Add feature: React frontend

--- backend/airbnbrental/urls.py
+++ backend/airbnbrental/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = 'AirBnb Rentals Admin'
+admin.site.index_title = 'Admin'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
